5871415e67cc3652385a537165e25b46d0269d40
ECecillo <enzo@batch.com>
2025-01-05 15:59:48 +0100

    feat: enforce reverse with validation

--- maths/reverse-numbers/main.go
+++ maths/reverse-numbers/main.go
@@ -1,6 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func validate(x int) bool {
+	bound := int(math.Pow(2, 31))
+
+	return x >= -bound && x <= bound
+}
 
 func reverse(x int) int {
 	var reverse, lastDigit int
@@ -15,6 +24,10 @@ func reverse(x int) int {
 		lastDigit = x % 10
 		reverse = (reverse * 10) + lastDigit
 		x = x / 10
+	}
+
+	if !validate(reverse) {
+		return 0
 	}
 
 	if isNegative {
